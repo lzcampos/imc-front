66e99aba053faaad5606f219036a9992cd0eea82
Luiz Damasceno <luizedc1@gmail.com>
2022-06-24 07:53:31 -0300

    Adding publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    allowedHosts: "all",
-  },
+  publicPath: process.env.NODE_ENV === "production" ? "/imc-front/" : "/",
 });
